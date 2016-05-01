c3e9d960c272f52afd498988232c8a29797c17b9
ahza64 <ahza64@yahoo.com>
2016-04-30 21:32:07 -0700

    edit complete, still trouble with new format in classes, set a different path that makes more sense

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -11,17 +11,44 @@ class CreaturesController < ApplicationController
   end
 
   def create
+    # gather data
     c_params = params.require(:creature).permit(:name, :description)
+    # add to db
     creature = Creature.new(c_params)
+    # if save success, direct to show page
     if creature.save
       redirect_to creature_path(creature)
     end
   end
 
   def show
+    # get id
     creature_id = params[:id]
+    # find creature and set var to use in view
     @creature = Creature.find_by_id(creature_id)
+    # show show page
     render :show
+  end
+
+  def edit
+    # get id
+    id = params[:id]
+    # find creature and set instanse var to use in view
+    @creature = Creature.find_by_id(id)
+    # show edit page
+    render :edit
+  end
+
+  def update
+    # get id
+    id = params[:id]
+    # find creature
+    creature = Creature.find_by_id(id)
+    # gather data
+    c = params.require(:creature).permit(:name, :description)
+    # add to db
+    creature.update_attributes(c)
+    redirect_to creatures_path
   end
 
 end
