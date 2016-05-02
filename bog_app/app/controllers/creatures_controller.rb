d8c8c43d501fbd71e5f43b5fce2437381b06d21e
ahza64 <ahza64@yahoo.com>
2016-05-01 17:55:05 -0700

    update complete

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -25,4 +25,18 @@ class CreaturesController < ApplicationController
     render :show
   end
 
+  def edit
+    id = params[:id]
+    @creature = Creature.find_by_id(id)
+    render :edit
+  end
+
+  def update
+    id = params[:id]
+    creature = Creature.find_by_id(id)
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
+
 end
