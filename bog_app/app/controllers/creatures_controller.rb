da4a2bcd92b0e13b08d511e9c01c48a01cfabe3f
ahza64 <ahza64@yahoo.com>
2016-05-01 17:45:05 -0700

    create complete

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -5,4 +5,24 @@ class CreaturesController < ApplicationController
     render :index
   end
 
+  def new
+    @creature = Creature.new
+    render :new
+  end
+
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    creature = Creature.new(creature_params)
+    if creature.save
+      redirect_to creature_path(creature)
+    end
+  end
+
+  def show
+    id = params[:id]
+    @creature = Creature.find_by_id(id)
+    render :show
+  end
+
 end
